0118cb6e53333062f509698bd711fb288acf705f
annusinha1 <annusinha15029@gmail.com>
2023-03-30 12:00:56 +0530

    Create Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body'
+    ];
+    
+    protected function title():Attribute
+    {
+        return new Attribute(
+            set:fn($value)=>[
+                'title'=> $value,
+                'slug'=> Str::class::slug($value)
+            ]
+        );
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
