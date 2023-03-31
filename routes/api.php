b4b6e23b23936c4a5e828e27344276c1fd7e3ae7
annusinha1 <annusinha15029@gmail.com>
2023-03-31 11:44:29 +0530

    Make UserController and Connect UserController in Routes->api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,9 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('all',[PostController::class, "getAllPublicPost"]);
 Route::post('createPost',[PostController::class, "createPost"]);
@@ -32,5 +33,8 @@ Route::post('delete', [CommentController::class,"delete"]);
 
 // Route::post('/post/{user_id}/comments/create',[CommentController::class,'create'])->middleware('auth:sanctum');
 
+Route::group(['middleware'=>'api'],function($routes){
 
+    Route::post('register',[UserController::class,"register"]);
+});
 
