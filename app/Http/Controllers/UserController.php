6d059b17e9091979909078594ea5db9d56a44d86
annusinha1 <annusinha15029@gmail.com>
2023-03-31 17:29:40 +0530

    fix create token error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,121 +5,98 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-   public function register(Request $request){
-
-    // $validator= Validator::make($request->all(),[
-    //   'name' => 'required|string|min:4|max:200',
-    //   'email' => 'required|string|email|max:200|unique',
-    //   'password' => 'required|string|min:6|confirmed'
-    // ]);
-
-    $validator  = Validator::make($request->all(), [
-        'name'  => 'required|string',
-        'email'      => 'required|unique:users,email|string',
-        'password'     => 'required|string|min:6|confirmed'
-    ]);
-
-
-    if($validator->fails())
-    {
-        return response()->json([
-            "code" => Response::HTTP_UNAUTHORIZED,
-            'status' => false,
-            'message' =>  ("Register Fails"),
-            "errors" => $validator->errors(),
-        ],Response::HTTP_UNAUTHORIZED);
-    }
-
-    $user= User::create([
-        'name' => $request->name,
-        'email' => $request->email,
-        'password' => Hash::make($request->password)
-    ]);
-
-    return response()->json([
-        'message' => 'User Successfully Registered',
-        'user' => $user
-    ]);
-
-   }
-
-
-   protected function loginErrors(): array
+   public function register(Request $request)
    {
-       return [
-           "email.required" => ("Email cannot be empty!"),
-           "email.email" => ("The email field must be a valid email address."),
-           "password.required" => ("The Password is required")
-       ];
-   }
+        $validator  = Validator::make($request->all(),[
+            'name'  => 'required|string',
+            'email'      => 'required|unique:users,email|string',
+            'password'     => 'required|string|min:6|confirmed'
+        ]);
 
 
-   public function login(Request $request)
-   {
-    $validator  = Validator::make($request->all(), [
-        'email'      => 'required|string|email',
-        'password'     => 'required|string|min:6'
-    ],$this->loginErrors());
+        if($validator->fails())
+        {
+            return response()->json($validator->errors(),400);
+        }
 
-
-    if($validator->fails())
-    {
-        return response()->json([
-            "code" => Response::HTTP_UNAUTHORIZED,
-            'status' => false,
-            'message' =>  ("Invalid format of email or password"),
-            "errors" => $validator->errors(),
-        ],Response::HTTP_UNAUTHORIZED);
-    }
-
-    // Tokenization point
-
-    $credentials = $request->only('email', 'password');
-    if (auth()->attempt($credentials, $request->filled('remember'))) {
-        $user = User::where(['email' => $request->email])->first();
+        $user= User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
         $user->token = $user->createToken(env("APP_TOKEN", ''))->plainTextToken;
+        
         return response()->json([
-            'code' => Response::HTTP_OK,
-            'status' => true,
-            "data" => $user,
-            "message" => ("User has been logged successfully.")
-        ], Response::HTTP_OK);
+            'message' => 'User Successfully Registered',
+            'user' => $user
+        ]);
+
+        
     }
 
-    return response()->json([
-        "code" => Response::HTTP_UNAUTHORIZED,
-        "status" => false,
-        "data" => [
-            "user" => [],
-        ],
-        "message" => ("Invalid email or password")
-    ], Response::HTTP_UNAUTHORIZED);
-}
-//     $token = auth()->attempt($validator->validated());
-//     if(!$token)
-//     {
-//         return response()->json([
-//             'error'=> "Unauthorized"
-//         ]);
-//     }
+    protected function loginErrors(): array
+    {
+        return [
+            "email.required" =>("Email cannot be empty!"),
+            "email.email" => ("The email field must be a valid email address."),
+            "password.required" => ("The Password is required")
+        ];
+    }
 
-//    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Illuminate\Http\Request $request
+     * 
+     * @return Illuminate\Http\JsonResponse
+     */
 
-//    protected function respondeWithToken($token)
-//    {
-//     return response()->json([
-//         'access_token' => $token,
-//         'token_type' => 'bearer',
-//         'expires_in' => auth()->attempt()->getTTL() * 60000
+    public function login(Request $request): JsonResponse
+    {
 
-//         // 'expire_in' => auth()->factory()->getTTL()*60
-//     ]); 
-//    }
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ], $this->loginErrors());
+
+        if ($validator->fails()) {
+            return response()->json([
+                "code" => Response::HTTP_UNAUTHORIZED,
+                'status' => false,
+                'message' => ("Invalid format of email or password"),
+                "errors" => $validator->errors(),
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        
+        $credentials = $request->only('email', 'password');
+
+        if (auth()->attempt($credentials, $request->filled('remember'))) {
+            $user = User::where(['email' => $request->email])->first();
+            $user->token = $user->createToken(env("APP_TOKEN", ''))->plainTextToken;
+            return response()->json([
+                'code' => Response::HTTP_OK,
+                'status' => true,
+                "data" => $user,
+                "message" => ("User has been logged successfully.")
+            ], Response::HTTP_OK);
+        }
+
+        return response()->json([
+            "code" => Response::HTTP_UNAUTHORIZED,
+            "status" => false,
+            "data" => [
+                "user" => [],
+            ],
+            "message" => ("Invalid email or password")
+        ], Response::HTTP_UNAUTHORIZED);
+    }
+
+
 }
